91b5809513dd8f0f93b342a769507b9645e00a32
ehsanghaffarii <ghafari.5000@gmail.com>
2026-07-14 07:39:00 +0330

    feat: add Yektanet analytics script to RootLayout for tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -150,6 +150,11 @@ export default function RootLayout({
             }),
           }}
         />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `!function(e,t,n){e.yektanetAnalyticsObject=n,e[n]=e[n]||function(){e[n].q.push(arguments)},e[n].q=e[n].q||[];var a=t.getElementsByTagName("head")[0],r=new Date,c="https://cdn.yektanet.com/superscript/OMFiTrDk/native-ui.eindev.ir-47365/yn_pub.js?v="+r.getFullYear().toString()+"0"+r.getMonth()+"0"+r.getDate()+"0"+r.getHours(),s=t.createElement("link");s.rel="preload",s.as="script",s.href=c,a.appendChild(s);var l=t.createElement("script");l.async=!0,l.src=c,a.appendChild(l)}(window,document,"yektanet");`,
+          }}
+        />
       </head>
       <body className="font-sans antialiased">
         <MotionProvider>
